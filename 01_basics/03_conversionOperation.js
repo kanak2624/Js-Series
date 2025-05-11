a50d89cd43f57a9bf9c04fc85d825a19fc443e40
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-11 06:55:55 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************** Operations ********************* //
+
+let value = 3
+let  negValue = -value
+// console.log(negValue); // -2
+
+// Arithematic Operation
+// console.log(2+2); // 4  Addition
+// console.log(2-2); // 0  Substraction
+// console.log(2*2); // 4  Multiply
+// console.log(2**5); // 32  Power
+// console.log(2/2); // 1    Division
+// console.log(2%2); // 0    Modulus
+
+
+let str1 = "hello"
+let str2 = " siddhi"
+
+let str3 = str1 + str2
+// console.log(str3); // hello siddhi
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2 // not proper way to write code it is very confusing
+
+let gameCounter = 100
+// gameCounter++; // 101
+// ++gameCounter // 102
+console.log(gameCounter); 
+
+
+
+
+
+ 
+
+
+
