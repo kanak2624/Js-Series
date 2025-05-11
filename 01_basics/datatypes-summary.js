34577a1d003538d5b461850aadf9ecc532d1dd84
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-11 08:44:23 +0000

    datatypes-summary-done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,54 @@
+// ********* DataTypes ********* //
+// DataTypes - 2 types 
+// primitive and non-primitive/reference
+ 
+// 1. Primitive : 7 types
+// String , Number , Boolean , Null , Undefined , Symbol , BigInt 
+
+// 2. Non-Primitive / Rerence :
+// Arrays , Objects , Functions
+
+
+// 1. Primitive : 7 types : Examples -
+const score = 100
+const scoreValue = 100.3 // typeof : number
+
+const isLoggedIn = false
+const outsideTemp = null // // typeof : object
+let userEmail;
+
+// values same provide nhi karta Symbol
+const id = Symbol("123")
+const anotherId = Symbol('123') // // typeof : symbol
+console.log(id === anotherId); // false
+
+// BigInt
+// const bigNumber = 344353465465676879n // n se represent horaha hai BigInt // typeof : undefined
+
+
+// 2. Non-Primitive / Rerence : 
+// Arrays , Objects , Functions
+// Examples
+const heros = ["shaktiman", "naagraj" , "doga"] // Array // typeof : object
+
+let myObj = {
+    name : "hitesh",
+    age : 22, 
+} // Object // typeof : object
+
+const myFunction  = function(){
+   //  console.log("Hello World");
+    
+} // Function // function // called as function object
+
+
+///////////////
+console.log(typeof bigNumber); // undefined
+console.log(typeof outsideTemp); // object
+console.log(typeof scoreValue); // number
+console.log(typeof myFunction); // function // called as function object
+console.log(typeof heros); // object
+console.log(typeof anotherId); // symbol
+console.log(typeof myObj); // object
+
+
