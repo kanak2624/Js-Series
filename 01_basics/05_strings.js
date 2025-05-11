eaff5afd478945c5ee9328717ae38542eddffa94
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-11 17:37:32 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "siddhi"
+const repoCount = 50
+
+// console.log( name + repoCount + " Value"); // siddhi50 Value
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // Hello my name is siddhi and my repo count is 50
+
+
+// String Declare //
+
+const gameName = new String('siddhi-developer-sharma-ji')  
+
+// console.log(gameName[0]); // s
+// console.log(gameName.__proto__); // {}
+// console.log(gameName.length); // 6
+// console.log(gameName.toUpperCase()); // Hello my name is siddhi and my repo count is 50
+                                        //SIDDHI
+// console.log(gameName.charAt(2)); // d
+// console.log(gameName.indexOf('d')); // 2
+
+const newString = gameName.substring(0,4) // last word not include 4-1 = 3 .
+// console.log(newString); // sidd
+
+const anotherString = gameName.slice(-3 , 5)
+// console.log(anotherString);
+
+const newStringOne = "    samriti    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); // starting spaces or ending remove // samriti
+
+const url = "https://siddhi.com/siddhi%20sharma"
+
+// console.log( url.replace('%20','-')); // https://siddhi.com/siddhi-sharma
+
+// console.log(url.includes('siddhi')); // true
+// console.log(url.includes('saurav')); // false
+
+// console.log(gameName.split('-')); // [ 'siddhi', 'developer', 'sharma', 'ji' ]
+
+
+ 
+
