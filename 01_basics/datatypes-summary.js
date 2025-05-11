3e9b44c42e59a65ba73b1104cb5c2084ae6fed57
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-11 13:02:05 +0000

    Stack And Heap  Memory Done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -20,7 +20,7 @@ let userEmail;
 // values same provide nhi karta Symbol
 const id = Symbol("123")
 const anotherId = Symbol('123') // // typeof : symbol
-console.log(id === anotherId); // false
+// console.log(id === anotherId); // false
 
 // BigInt
 // const bigNumber = 344353465465676879n // n se represent horaha hai BigInt // typeof : undefined
@@ -42,13 +42,37 @@ const myFunction  = function(){
 } // Function // function // called as function object
 
 
-///////////////
-console.log(typeof bigNumber); // undefined
-console.log(typeof outsideTemp); // object
-console.log(typeof scoreValue); // number
-console.log(typeof myFunction); // function // called as function object
-console.log(typeof heros); // object
-console.log(typeof anotherId); // symbol
-console.log(typeof myObj); // object
+///////////////////////////////////////
+// console.log(typeof bigNumber); // undefined
+// console.log(typeof outsideTemp); // object
+// console.log(typeof scoreValue); // number
+// console.log(typeof myFunction); // function // called as function object
+// console.log(typeof heros); // object
+// console.log(typeof anotherId); // symbol
+// console.log(typeof myObj); // object
 
+
+
+//////// ************* Stack and Heap Memory *************** ///////////////
+// Stach (Primitive use) , Heap (Non-primitive)
+// Stach (Primitive use) : it gives us copy value .
+// Heap (Non-primitive) : it gives us original value reference . 
+let myName = "siddhisharma" // variable is a primitive type so it comes in Stack Memory
+
+let anotherName = myName
+anotherName = "siddhiaurcode"
+
+console.log(myName); 
+console.log(anotherName); 
+
+
+// Heap (Non-primitive)
+let user1= {
+    email : "user@google.com",
+    upi : "useer@ybi", 
+}
+let user2 = user1
+user2.email = "siddhi@gmail.com" // value excess kar sakte hai . se
+console.log(user1.email); // same output in both  // siddhi@gmail.com
+console.log(user2.email); // same output in both  // siddhi@gmail.com
 
