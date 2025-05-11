9c612fc4ce95fbede7833c6a7c749b56e11a2649
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-11 07:16:33 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// *************** Comparision Operator ************* //
+
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // conversion problem aate hai yaha
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+
+// strict check === also check its dataType
+
+// console.log("2" === 2); // false
+
+
