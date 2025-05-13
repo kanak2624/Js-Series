e9b847473053cfd032c8dc212ece8e2f41964b78
Siddhi Sharma <133845253+kanak2624@users.noreply.github.com>
2025-05-13 05:45:55 +0000

    Date done

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -9,7 +9,7 @@ let myDate = new Date()
 // console.log(myDate.toLocaleDateString()); // 5/12/2025
 // console.log(myDate.toLocaleString()); // 5/12/2025, 8:06:21 AM
 // console.log(myDate.toLocaleTimeString()); // 8:06:39 AM
-console.log(typeof myDate); // object
+// console.log(typeof myDate); // object
 
 
 let myCreatedDate = new Date(2023, 0, 23) // o means month // js mai 0 se month start hota hai
@@ -25,5 +25,18 @@ let myNewDateee= new Date("01-14-2023")
 // console.log(myNewDateee.toLocaleString()); // 1/14/2023, 12:00:00 AM
 
 let myTimeStamp = Date.now() // quizes , codes use
-console.log(myTimeStamp); // 1747071148199 // miliseconds
+// console.log(myTimeStamp); // 1747071148199 // miliseconds
 
+// console.log(Date.now()/1000); // milisecs mai value dega // sec mai convert karne ke liye divide by 1000 but yeah decimal mai value dega so we use this see in next line // 1747113969.539
+
+// console.log(Math.floor(Date.now()/1000)); // 1747114477 // isme value decimal mai nhi aayegi but milisecs mai dega
+
+let AssumeDate = new Date()
+// console.log(AssumeDate); // 2025-05-13T05:38:39.994Z
+// console.log(AssumeDate.getDate()); // 13
+// console.log(AssumeDate.getDay());  // 2
+
+// String interpollation
+// `${AssumeDate.getHours()} and the hours is ` 
+
+AssumeDate.toLocaleString('')
